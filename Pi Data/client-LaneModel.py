a9e735a27b595b9b694c99690becec1126202324
Lamchopp <michael200207@gmail.com>
2024-12-02 23:30:39 -0500

    lane-model works with python3.9 conda env and venv deactivated

--- Pi Data/client-LaneModel.py
+++ Pi Data/client-LaneModel.py
@@ -2,12 +2,24 @@ import socketio
 import random
 import time
 
+from MotorModule import Motor
+import cv2
+import cameraModule as cM
+
 '''
 How the client should interact with the lane detection/deep learning model
 '''
 
+##################################
+###Define Motor Config###
+# Pin Order for motor class connected to L298N board: EnA In1 In2 (left), EnB, In3, In4(right)
+motor = Motor(2,3,4, 22,17,27)
+##################################
+steer_data = None
+
 # Connect to the server
 sio = socketio.Client()
+server_url = "http://10.0.0.184:5000"
 
 # This will run when we are connected to the server
 @sio.event
@@ -20,7 +32,10 @@ def connect():
 # Here we are getting the calculated steering angle value with AI
 @sio.on('steer')
 def on_steer(data):
+    global steer_data
+    steer_data = data['steering_angle']
     print('Got steering data:', data)
+
 
 # When we disconnect from the server
 @sio.event
@@ -30,20 +45,42 @@ def disconnect():
 # Emit telemetry data 5 times per second. Here we are sending the car data to the server to then calculate a steering angle for us.
 def emit_telemetry():
     while True:
+
+        frame = cM.getImgR(False, [680,440])
+        global steer_data
+
+        if steer_data is not None:
+            print("Telemetry using control data:", steer_data)
+            motor.move(0.5, steer_data['steerAngle'], 0.1)
+
+        else:
+            print("No control data received yet.")
+
+        # Encode frame as JPEG
+        _, img_encoded = cv2.imencode('.jpg', frame)
+
+        img_encoded = img_encoded.tobytes()
+
         # Simulating telemetry data
         telemetry_data = {
-            'speed': round(random.uniform(0, 100), 2),  # sending a random speed
+            'image': img_encoded,  # sending a random speed
             # In the real Unity simulator, the images from 3 cameras should also be sent
         }
-
+        # Send the frame to the server
+        sio.emit('telemetry', telemetry_data)
         print('Emitting telemetry data:', telemetry_data)
 
-        # Send telemetry event to the server
-        sio.emit('telemetry', telemetry_data)
 
+        # Display the frame locally
+        cv2.imshow('Raspberry Pi Camera', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        
         # Wait for 0.2 seconds (or as needed)
         time.sleep(0.2)
+    cv2.destroyAllWindows()
+    sio.disconnect()
 
 # Connect to the server
-sio.connect('http://localhost:4567')
+sio.connect(server_url)
 sio.wait()
